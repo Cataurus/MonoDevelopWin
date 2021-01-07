bee9f4b04e45eacb71837ce805888527f321f0ad
Ulf Prill <cataurusfynn@yahoo.de>
2021-01-07 03:32:04 +0100

    Update Winbuild.bat

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -41,7 +41,7 @@ set "PLATFORM=Any CPU"
 "%MSBUILD_EXE%" -fl1 -fl2 -fl3 -flp1:Summary;Encoding=UTF-8;logfile=MonoDevelopBuild.Summary.log -flp2:errorsonly;logfile=MonoDevelopBuild.JustErrors.log;verbosity=detailed;Encoding=UTF-8 -flp3:warningsonly;verbosity=detailed;Encoding=UTF-8;logfile=MonoDevelopBuild.JustWarnings.log /r /m "/p:Configuration=%CONFIG%" "/p:Platform=%PLATFORM%" %* || goto :error
 
 REM Build Installer
-"%MSBUILD_EXE%" ../setup/setup.sln -fl1 -fl1:MonoDevelopSetup.log "/p:Configuration=Debug" "/p:Platform=x86" %* || goto :error
+"%MSBUILD_EXE%" ../setup/setup.sln -fl1 -flp1:MonoDevelopSetup.log "/p:Configuration=Debug" "/p:Platform=x86" %* || goto :error
 
 goto :eof
 

2cc4aa384510fa5a233f58bd5b3627462239899c
Ulf Prill <cataurusfynn@yahoo.de>
2021-01-11 21:28:25 +0100

    update

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -31,7 +31,7 @@ exit /b 1
 
 git submodule sync || goto :error
 git submodule update --init --recursive || goto :error
-REM "external\nuget-binary\NuGet.exe" restore Main.sln
+"external\nuget-binary\NuGet.exe" restore Main.sln
 
 "%MSBUILD_EXE%" external\fsharpbinding\.paket\paket.targets /t:RestorePackages /p:PaketReferences="%~dp0external\fsharpbinding\MonoDevelop.FSharpBinding\paket.references"
 
